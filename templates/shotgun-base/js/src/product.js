d3a340257fd0e3a72305483348b5a61a981fd9c2
Neil Grey <neil.grey@shotgunsoftware.com>
2017-05-31 10:50:43 -0700

    Removing integrations from grey dropdown as it's confusing

--- templates/shotgun-base/js/src/product.js
+++ templates/shotgun-base/js/src/product.js
@@ -38,7 +38,7 @@ function getProductMenu(categoryId) {
 
   /* Replace this with index articles instead of categories */
   var menuType = "categories";
-  var menuOrder = ["202726378","115000022673","202726418"];
+  var menuOrder = ["202726378","202726418"];
   /* Studio */
   if (validateCategoryAccess("202726408")) {
       menuOrder.push("202726408");
@@ -53,9 +53,6 @@ function getProductMenu(categoryId) {
   for (var i=0; i<menuOrder.length; i++) {
     if (productMap.hasOwnProperty(menuOrder[i])) {
       productName = productMap[menuOrder[i]];
-      if (menuOrder[i] == "115000022673") {
-        productName = "Integrations";
-      }
       menuHtml += '<a href="https://support.shotgunsoftware.com/hc/en-us/' + menuType + '/' + menuOrder[i] + '" rel="nofollow" role="menuitem">' + productName + '</a>';
     }
   }
